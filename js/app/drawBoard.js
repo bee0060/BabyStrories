85f7d6bf755948e3d94d365c002fca32a73fbfc7
Steven Weng <Steven_Weng@epam.com>
2016-01-11 18:07:25 +0800

    start add board

--- js/app/drawBoard.js
+++ js/app/drawBoard.js
@@ -1,4 +1,29 @@
-angular.module('drawBoard').controller('drawBoardCtrl', function($scope){
+'use strict';
 
+
+var drawBoard = angular.module('drawBoard', []);
+
+drawBoard.controller('drawBoardCtrl', function($scope) {
 	$scope.name = "Board";
+	$scope.blockes = [];
+
+
+
+	(function() {
+		var blockes = new Array(81);
+
+		for (var i = 0, len = 81; i < len; i++) {
+			blockes[i] = false;
+		}
+		$scope.blockes = blockes;
+		$scope.selectBlock = selectBlock;
+
+
+	})();
+
+	function selectBlock(blockes, index) {
+		// blockes[index] = 1 - blockes[index];
+		blockes[index] = !blockes[index];
+	}
+
 });
